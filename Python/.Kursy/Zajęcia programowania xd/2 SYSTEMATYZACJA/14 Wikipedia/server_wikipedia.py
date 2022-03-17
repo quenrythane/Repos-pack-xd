932d1c3947e94930dca2dc7ca08e171d4711fc19
quenrythane <artur.babinski.g@gmail.com>
2022-03-17 11:44:33 +0100

    done homework ZPXD 15 - draw 3 Naruto characters and display them in sorted way with sort by len of summary (the longest first)

--- Python/.Kursy/Zajęcia programowania xd/2 SYSTEMATYZACJA/14 Wikipedia/server_wikipedia.py
+++ Python/.Kursy/Zajęcia programowania xd/2 SYSTEMATYZACJA/14 Wikipedia/server_wikipedia.py
@@ -2,7 +2,14 @@ from pliki_pythonowe.my_wikipedia import *
 
 # @app.route('/naruto')
 def naruto():
-    heroes_list = collect_heroes_list("dane/super_heroes.json")
+    heroes_list = collect_heroes_list("data/super_heroes.json")
     random_hero = draw_random_hero(heroes_list)
     name, img, summary = find_character_info(random_hero)
+    sorted_words_by_length = summary
     return name, img, summary
+
+heroes_list = [naruto(), naruto(), naruto()]
+# wylosuj 3 postacie i postuj je aby te największą liczbą wyrazów były pierwsze
+sorted_heroes = sorted(heroes_list, key=lambda hero: len(hero[2]), reverse=True)
+for name, x, summary in sorted_heroes:
+    print(name, len(summary))
