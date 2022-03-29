99d32f6e313a1b1662e3c42b1331e4be5feb0815
Artur Babinski <46361319+quenrythane@users.noreply.github.com>
2022-03-29 20:54:21 +0200

    Update greatest_common_divisor.py

--- Python/Kursy/Zajęcia programowania xd/Wtorki z Teorią/Zajęcia 2/greatest_common_divisor.py
+++ Python/Kursy/Zajęcia programowania xd/Wtorki z Teorią/Zajęcia 2/greatest_common_divisor.py
@@ -5,7 +5,7 @@ https://pl.wikipedia.org/wiki/Algorytm_Euklidesa
 
 
 def euklides(a, b):
-    a, b = max(a, b), min(a, b)  # set a as bigger number, b as smaller
+    # a, b = max(a, b), min(a, b)  # set a as bigger number, b as smaller  # it's not necessary  cause they will change positions if a > b
     r = 1
     while r != 0:
         r = a % b
